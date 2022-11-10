6a381f5a0786aead4c182f61b061836e25fd8a60
Shayan Mahtabi <shayanmahtabi@Shayans-MacBook-Pro.fritz.box>
2022-11-11 00:14:30 +0100

    Everything is set back to it's original state

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
 import './App.scss';
-
-import Navbar from './Components/Navbar/Navbar';
-import Footer from './Components/Footer/Footer';
 
 function App() {
 
   return (
-    <div className='landingpage-hero'>
-      <Navbar />
-      <div className="container hero-section">
-        <div className="row col-12">
-          <h1 className='text-white'>Das A Team</h1>
-          <span className='text-white motto'>Innovate, Connect, Inspire</span>
-          <br />
-          <h3 className='text-white mt-2'>Wir entwickeln "Das Verrückte Labyrinth"</h3>
-        </div>
-      </div>
-      <Footer />
-    </div>
+    <>
+      Hello World! I'm using React with TailwindCSS and Material UI
+    </>
   );
 }
 
