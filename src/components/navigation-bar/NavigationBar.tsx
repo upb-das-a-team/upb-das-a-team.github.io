cb22af20ffc2ad8cb9fe9d272b69d7463a5f4b9c
Shayan Mahtabi <shayanmahtabi@Shayans-MacBook-Pro.fritz.box>
2022-11-11 22:51:09 +0100

    NavigationBar is modified and tested

--- src/components/navigation-bar/NavigationBar.tsx
+++ src/components/navigation-bar/NavigationBar.tsx
@@ -1,12 +1,16 @@
 import React from "react";
+import { useState } from "react";
 
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 import Logo from '../../assets/images/logo-icon-bg-white.svg';
 
 import './NavigationBar.scss';
 
 function NavigationBar() {
+  const [currentLocation, setCurrentLocation] = useState(window.location.pathname);
+  useLocation();
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
@@ -18,15 +22,15 @@ function NavigationBar() {
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
-            <li className="nav-item">
+            <li className={"nav-item " + (window.location.pathname === '/' ? "activated" : "")}>
               <Link className="nav-link" to={'/'}>HOME</Link>
             </li>
             <span className="navbar-nav--divider"></span>
-            <li className="nav-item">
+            <li className={"nav-item " + (window.location.pathname === '/project' ? "activated" : "")}>
               <Link className="nav-link" to={'/project'}>PROJECT</Link>
             </li>
             <span className="navbar-nav--divider"></span>
-            <li className="nav-item">
+            <li className={"nav-item " + (window.location.pathname === '/team' ? "activated" : "")}>
               <Link className="nav-link" to={'/team'}>TEAM</Link>
             </li>
           </ul>
