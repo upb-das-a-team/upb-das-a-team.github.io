cd44150d61fecbe1201d3451e34fc03ce0d61ea8
Shayan Mahtabi <shayanmahtabi@Shayans-MacBook-Pro.fritz.box>
2022-11-11 22:52:38 +0100

    Index files are updated

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.scss';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/scss/bootstrap.scss';
+import './index.scss';
 
-import 'bootstrap/dist/js/bootstrap';
+import AppRouter from './components/app-router/AppRouter';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <AppRouter />
   </React.StrictMode>
 );
 
