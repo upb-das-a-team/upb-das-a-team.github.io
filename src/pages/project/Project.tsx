ce3bb6d06bae0af5edc64ae4e287be6ac7a49528
Shayan Mahtabi <shayanmahtabi@Shayans-MacBook-Pro.local>
2022-11-13 12:03:13 +0100

    ProjectPage main files initialised

--- src/pages/project/Project.tsx
+++ src/pages/project/Project.tsx
@@ -2,10 +2,63 @@ import React from "react";
 
 import './Project.scss';
 
+import GanttChart from "../../components/gantt-chart/GanttChart";
+import TimelineDevelopment from "../../components/timeline-development/TimelineDevelopment";
+
 function Project() {
+
   return(
     <>
-      <h1>Project</h1>
+      <div className="projectpage">
+        <div className="container">
+          <div className="row projectpage-header">
+            <h1 className="title">Das Verrückte Labyrinth</h1>
+            <p className="description">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+              Nostrum quos modi repellat ipsum accusamus amet minima iure dolore autem, 
+              aspernatur pariatur mollitia quis fuga explicabo assumenda atque voluptates! Aperiam, magnam!
+
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+              Nostrum quos modi repellat ipsum accusamus amet minima iure dolore autem, 
+              aspernatur pariatur mollitia quis fuga explicabo assumenda atque voluptates! Aperiam, magnam!
+            </p>
+          </div>
+        </div>
+
+        <div className="container">
+          <div className="row projectpage-timeline">
+            <h2 className="title">Projekt Zeitplan</h2>
+            <p className="description">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+              Eveniet aspernatur autem quidem. Laboriosam harum amet tenetur deleniti maiores ut dolor, 
+              aliquid quos nihil soluta aperiam ab ad nostrum omnis? Ipsam?
+            </p>
+          </div>
+        </div>
+
+        <div className="container-fluid">
+          <div className="row project-gantt-chart">
+            <GanttChart />
+          </div>
+        </div>
+
+        <div className="container">
+          <div className="row projectpage-development">
+            <h2 className="title">Projekt Entwicklung</h2>
+            <p className="description">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+              Eveniet aspernatur autem quidem. Laboriosam harum amet tenetur deleniti maiores ut dolor, 
+              aliquid quos nihil soluta aperiam ab ad nostrum omnis? Ipsam?
+            </p>
+          </div>
+        </div>
+
+        <div className="container-fluid">
+          <div className="row project-sprint-timeline">
+            <TimelineDevelopment />
+          </div>
+        </div>
+      </div>
     </>
   )
 }
