68711caa4a3e1a0fa4e1954b839547de2a1f613e
Shayan Mahtabi <shayanmahtabi@Shayans-MBP.fritz.box>
2022-12-02 00:02:50 +0100

    TeamPage is updated

--- src/pages/team/Team.tsx
+++ src/pages/team/Team.tsx
@@ -1,18 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import { AiOutlineCopyrightCircle, AiOutlineGitlab } from "react-icons/ai";
+import { ImSection } from "react-icons/im";
+import { Link } from "react-router-dom";
 
 import MemberCard from "../../components/member-card/MemberCard";
 
 import './Team.scss';
 
 function Team() {
-  const members = [
+  const [currentDate, setCurrentDate] = useState(new Date())
+
+  const members01 = [
     {
       img: 'RobinEkmen.png',
       name: 'Robin Ekmen',
       email: 'rekmen@mail.uni-paderborn.de',
       gitlab: 'https://git.cs.uni-paderborn.de/rekmen',
       role: 'Scrum Master, Developer',
-      motto: 'lorem ipsum'
+      motto: 'If you tell the truth, you don\'t have to remember anything.'
     },
     {
       img: 'ShayanMahtabi.png',
@@ -20,7 +25,7 @@ function Team() {
       email: 'smahtabi@mail.uni-paderborn.de',
       gitlab: 'https://git.cs.uni-paderborn.de/smahtabi',
       role: 'Product Owner, Developer',
-      motto: 'lorem ipsum'
+      motto: 'Courage is grace under pressure.'
     },
     {
       img: 'NiklasBäumker.png',
@@ -28,15 +33,18 @@ function Team() {
       email: 'niba@mail.uni-paderborn.de',
       gitlab: 'https://git.cs.uni-paderborn.de/niba',
       role: 'Quality Manager, Developer',
-      motto: 'lorem ipsum'
-    },
+      motto: 'Life is what happens to you while you\'re busy making other plans.'
+    }
+  ]
+
+  const members02 = [
     {
       img: 'LukasKrämer.png',
       name: 'Lukas Krämer',
       email: 'lukr@mail.uni-paderborn.de',
       gitlab: 'https://git.cs.uni-paderborn.de/lukr',
       role: 'Quality Manager, Developer',
-      motto: 'lorem ipsum'
+      motto: 'Work as intensely as you play and play as intensely as you work.'
     },
     {
       img: 'JakobKramer.png',
@@ -44,7 +52,7 @@ function Team() {
       email: 'jkramer3@mail.uni-paderborn.de',
       gitlab: 'https://git.cs.uni-paderborn.de/jkramer3',
       role: 'Test Manager, Developer',
-      motto: 'lorem ipsum'
+      motto: 'Do not accept anything because it comes from the mouth of a respected person.'
     },
     {
       img: 'AlexanderScheubel.png',
@@ -52,15 +60,18 @@ function Team() {
       email: 'lscheub@mail.uni-paderborn.de',
       gitlab: 'https://git.cs.uni-paderborn.de/lscheub',
       role: 'Test Manager, Developer',
-      motto: 'lorem ipsum'
-    },
+      motto: 'The important thing is not to stop questioning.'
+    }
+  ]
+
+  const members03 = [
     {
       img: 'ChristophRode.png',
       name: 'Christoph Rode',
       email: 'rodec@mail.uni-paderborn.de',
       gitlab: 'https://git.cs.uni-paderborn.de/rodec',
       role: 'Product Manager, Developer',
-      motto: 'lorem ipsum'
+      motto: 'Give up control. You never really had it anyway.'
     },
     {
       img: 'AndreasSittner.png',
@@ -68,7 +79,7 @@ function Team() {
       email: 'asittner@mail.uni-paderborn.de',
       gitlab: 'https://git.cs.uni-paderborn.de/asittner',
       role: 'Tools Manager, Developer',
-      motto: 'lorem ipsum'
+      motto: 'Never do the impossible. People will expect you to do it forever after.'
     },
     {
       img: 'KaiZingler.png',
@@ -76,15 +87,18 @@ function Team() {
       email: 'kzingler@mail.uni-paderborn.de',
       gitlab: 'https://git.cs.uni-paderborn.de/kzingler',
       role: 'Tools Representative, Developer',
-      motto: 'lorem ipsum'
-    },
+      motto: 'An investment in knowledge always pays the best interest.'
+    }
+  ]
+
+  const members04 = [
     {
       img: 'YittingJiang.png',
       name: 'Yitting Jiang',
       email: 'yitting@mail.uni-paderborn.de',
       gitlab: 'https://git.cs.uni-paderborn.de/yitting',
       role: 'Documents Manager, Developer',
-      motto: 'lorem ipsum'
+      motto: 'The greatest of all weaknesses is the fear of appearing weak.'
     },
     {
       img: 'CharlieLegrandDonfack.png',
@@ -92,7 +106,7 @@ function Team() {
       email: 'donfack@mail.uni-paderborn.de',
       gitlab: 'https://git.cs.uni-paderborn.de/donfack',
       role: 'Documents Representative, Developer',
-      motto: 'lorem ipsum'
+      motto: 'Luck is where preparation meets opportunity.'
     },
     {
       img: 'Joel-MauriceKessler.png',
@@ -100,16 +114,19 @@ function Team() {
       email: 'kessljo@mail.uni-paderborn.de',
       gitlab: 'https://git.cs.uni-paderborn.de/kessljo',
       role: 'Comittee Member, Developer',
-      motto: 'lorem ipsum'
-    },
+      motto: 'No one is all evil. Everybody has a good side. If you keep waiting, it will comme up.'
+    }
+  ]
+
+  const members05 = [
     {
       img: 'GhaithHalima.png',
       name: 'Ghaith Halima',
       email: 'ghalima@mail.uni-paderborn.de',
       gitlab: 'https://git.cs.uni-paderborn.de/ghalima',
       role: 'Developer',
-      motto: 'lorem ipsum'
-    },
+      motto: 'Perpetual optimism is a force multiplier.'
+    }
   ]
 
   return(
@@ -130,19 +147,103 @@ function Team() {
         </div>
       </div>
 
-      <div className="container members">
-        {members.map((member) => {
-          return(
-            <MemberCard
-              img={member.img}
-              name={member.name}
-              email={member.email}
-              gitlab={member.gitlab}
-              role={member.role}
-              motto={member.motto}
-            />
-          )
-        })}
+      <div className="container">
+        <div className="members">
+          <div className="card-group">
+            {members01.map((member) => {
+              return(
+                <MemberCard
+                img={member.img}
+                name={member.name}
+                email={member.email}
+                gitlab={member.gitlab}
+                role={member.role}
+                motto={member.motto}
+                />
+              )
+            })}
+          </div>
+          <div className="empty-space"></div>
+          <div className="card-group">
+            {members02.map((member) => {
+              return(
+                <MemberCard
+                img={member.img}
+                name={member.name}
+                email={member.email}
+                gitlab={member.gitlab}
+                role={member.role}
+                motto={member.motto}
+                />
+              )
+            })}
+          </div>
+          <div className="empty-space"></div>
+          <div className="card-group">
+            {members03.map((member) => {
+              return(
+                <MemberCard
+                img={member.img}
+                name={member.name}
+                email={member.email}
+                gitlab={member.gitlab}
+                role={member.role}
+                motto={member.motto}
+                />
+              )
+            })}
+          </div>
+          <div className="empty-space"></div>
+          <div className="card-group">
+            {members04.map((member) => {
+              return(
+                <MemberCard
+                img={member.img}
+                name={member.name}
+                email={member.email}
+                gitlab={member.gitlab}
+                role={member.role}
+                motto={member.motto}
+                />
+              )
+            })}
+          </div>
+          <div className="empty-space"></div>
+          <div className="card-group">
+            {members05.map((member) => {
+              return(
+                <MemberCard
+                img={member.img}
+                name={member.name}
+                email={member.email}
+                gitlab={member.gitlab}
+                role={member.role}
+                motto={member.motto}
+                />
+              )
+            })}
+          </div>
+        </div>
+      </div>
+
+      <div className="homepage-footer">
+        <div className="homepage-footer-left">
+          <Link className="custom-link section-symbol" to={'/impressum'}>
+            <ImSection />
+          </Link>
+        </div>
+        <div className="homepage-footer-center">
+          <div className="footer-copyright-icon"><AiOutlineCopyrightCircle /></div>
+          <div className="footer-current-year">{currentDate.getFullYear()}</div>
+          <div className="footer-team-name">Das A Team</div>
+        </div>
+        <div className="homepage-footer-right">
+          <a className= "custom-link" href="https://git.cs.uni-paderborn.de/swtpra2223/gruppe11/" target={'_blank'}>
+            <div className="footer-gitlab-icon">
+              <AiOutlineGitlab />
+            </div>
+          </a>
+        </div>
       </div>
     </div>
   )
